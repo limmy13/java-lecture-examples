fd84502b77c8804850e5821643232d6e4def8355
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-06-27 21:29:54 -0500

    finished lecture example

--- LectureExamples/src/test/java/class06/beverages/SodaTest.java
+++ LectureExamples/src/test/java/class06/beverages/SodaTest.java
@@ -7,13 +7,39 @@ import static org.junit.Assert.*;
 
 public class SodaTest {
 
-    // TODO: Declare two objects of the Soda class (but do not initialize)
+    Soda rootBeer;
+    Soda pepsi;
 
-
-    // TODO: Define a @Before block to instantiate the two Soda objects declared above
-
+    @Before
+    public void createTestData() {
+        rootBeer = new Soda("Root Beer", 20, "bottle", false);
+        pepsi = new Soda("Pepsi", 12, "can", true);
+    }
 
     // TODO: Use the test data above to write 3 tests confirming the inherited properties are set
 
+    @Test
+    public void nameFieldIsInherited() {
+        String nameSpec = "a new Soda object is given a name through the Beverage class constructor";
+        String expectedName = "Root Beer";
+        String actualName = rootBeer.getName();
+        assertEquals(nameSpec, expectedName, actualName);
+    }
+
+    @Test
+    public void ouncesFieldIsInherited() {
+        String ouncesSpec = "a new Soda object is given ounces through the Beverage class constructor";
+        int expectedOunces = 20;
+        int actualOunces = rootBeer.getOunces();
+        assertEquals(ouncesSpec, expectedOunces, actualOunces);
+    }
+
+    @Test
+    public void containerFieldIsInherited() {
+        String containerSpec = "a new Soda object is given a container through the Beverage class constructor";
+        String expectedContainer = "can";
+        String actualContainer = pepsi.getContainer();
+        assertEquals(containerSpec, expectedContainer, actualContainer);
+    }
 
 }
