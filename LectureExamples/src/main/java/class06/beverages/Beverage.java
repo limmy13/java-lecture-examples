456fb08ea69b5ac7c4a8cf8808744982c018b71f
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-06-27 19:19:01 -0500

    lecture

--- LectureExamples/src/main/java/class06/beverages/Beverage.java
+++ LectureExamples/src/main/java/class06/beverages/Beverage.java
@@ -1,7 +1,7 @@
 package class06.beverages;
 
 // TODO: Convert the Beverage class to be abstract
-public class Beverage {
+public abstract class Beverage {
 
     private static int nextId = 1;
     private final int id;
@@ -20,9 +20,35 @@ public class Beverage {
 
     // TODO: Add getters & setters as appropriate
     //  - modify the setter for recycled to print a confirmation to the console if true
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getOunces() {
+        return ounces;
+    }
+
+    public String getContainer() {
+        return container;
+    }
+
+    public boolean isRecycled() {
+        return recycled;
+    }
+
+    public void setRecycled(boolean recycled) {
+        if (recycled) {
+            System.out.println("The " + this + " has been recycled.\n");
+        }
+        this.recycled = recycled;
+    }
 
     // TODO: Override toString() but make it abstract so it will have to be defined in the subclasses
-
+    @Override
+    public abstract String toString();
 
 }
